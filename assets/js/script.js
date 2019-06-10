d9cc46203e2297f44b4bd684e22ad66f0b34caf2
coco <coco@lacueva.tv>
2019-06-10 15:29:56 -0300

    first init

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    console.log('ready')
+    
+
+});
+
+window.addEventListener('load', function() {
+    //setea la altura al comparador de procesadores
+    console.log('all loaded');
+});
