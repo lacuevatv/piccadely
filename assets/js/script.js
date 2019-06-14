945b4fc9b80c2b824df6f705c0a2598b3f25ee75
coco <coco@lacueva.tv>
2019-06-14 15:47:05 -0300

    maquetado navbar con script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,40 @@
 document.addEventListener('DOMContentLoaded', function() {
-
     console.log('ready')
-    
+
+    $menu = document.getElementById('toggle');
+    $menu.addEventListener('click', function(){
+        openCloseMobileMenu();
+    });
+
 
 });
 
 window.addEventListener('load', function() {
-    //setea la altura al comparador de procesadores
     console.log('all loaded');
 });
+
+/*
+ * FUNCIONES
+*/
+function openCloseMobileMenu() {
+    $toggle = document.getElementById('toggle');
+    $menu = document.getElementById('navMobile');
+    
+    if ( $toggle.getAttribute('aria-expanded') === 'false' ) {
+        //ABRIR MENU
+        //modifica el aria
+        $toggle.setAttribute('aria-expanded', 'true');
+        //agrega la clase al toggle
+        $toggle.classList.add('menu-open');
+        //agrega la clase que abre el menu
+        $menu.classList.add('opened');
+    } else {
+        //CERRAR MENU
+        //modifica el aria
+        $toggle.setAttribute('aria-expanded', 'false');
+        //quita la clase al toggle
+        $toggle.classList.remove('menu-open');
+        //quita la clase al menu para que se cierre
+        $menu.classList.remove('opened');
+    }
+}//openCloseMobileMenu()
